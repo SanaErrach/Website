42bf5aa3bfc713be2d7457b864b34f12530db349
Smart Home project <zakaria.bensalem.1@gmail.com>
2020-03-23 00:19:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,394 @@
+<?php include 'form.php';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>My Website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+  
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center">
+          
+          <div class="col-6 col-xl-2">
+            <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">My Website</a></h1>
+          </div>
+
+          <div class="col-12 col-md-10 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="#about-section" class="nav-link">About</a></li>
+                <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                <li><a href="login.php" class="link">Login</a></li>
+                
+              </ul>
+            </nav>
+          </div>
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+
+    
+    <div class="site-block-wrap">
+    <div class="owl-carousel with-dots">
+      <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/hero_1.jpg);" data-aos="fade" id="home-section">
+
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 mt-lg-5 text-center">
+              <h1 class="text-shadow">Buy Property Here</h1>
+              <p class="mb-5 text-shadow">You can always find something you want. </p>  
+            </div>
+          </div>
+        </div>  
+      </div>  
+  
+      <div class="site-blocks-cover overlay overlay-2" style="background-image: url(images/hero_2.jpg);" data-aos="fade" id="home-section">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 mt-lg-5 text-center">
+              <h1 class="text-shadow">TREAT YOURSELF!</h1>
+              <p class="mb-5 text-shadow">Buy what you don’t have yet or what you really want, which can be mixed with what you already own. Buy only because something excites you, not just for the simple act of shopping.</p>   
+            </div>
+          </div>
+        </div>  
+      </div>  
+    </div>   
+  </div>      
+
+  <div class="site-section" id="properties-section">
+      <div class="container">
+        <div class="row large-gutters">
+          
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                  <div class="ftco-media-1-inner">
+                    <a href="#" class="d-inline-block mb-4"><img src="images/suit.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="ftco-media-details">
+                      <h3 style="font-size: 30px; font-family:  monospace">SUITS</h3>
+                      
+                    </div>
+      
+                  </div> 
+                </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                  <div class="ftco-media-1-inner">
+                    <a href="#" class="d-inline-block mb-4"><img src="images/dress.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="ftco-media-details">
+                      <h3 style="font-size: 30px; font-family:  monospace">DRESS</h3>
+                      
+                    </div>
+      
+                  </div> 
+                </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="#" class="d-inline-block mb-4"><img src="images/socks.jpg" alt="Image" class="img-fluid"></a>
+                  <div class="ftco-media-details">
+                    <h3 style="font-size: 30px; font-family:  monospace">SOCKS</h3>
+                    
+                  </div>
+    
+                </div> 
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                <div class="ftco-media-1">
+                    <div class="ftco-media-1-inner">
+                      <a href="#" class="d-inline-block mb-4"><img src="images/pant.jpg" alt="Image" class="img-fluid"></a>
+                      <div class="ftco-media-details">
+                        <h3 style="font-size: 30px; font-family:  monospace">PANTS</h3>
+                       
+                      </div>
+        
+                    </div> 
+                  </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                <div class="ftco-media-1">
+                    <div class="ftco-media-1-inner">
+                      <a href="#" class="d-inline-block mb-4"><img src="images/hoodie.jpg" alt="Image" class="img-fluid"></a>
+                      <div class="ftco-media-details">
+                        <h3 style="font-size: 30px; font-family:  monospace">HOODIES</h3>
+                       
+                      </div>
+        
+                    </div> 
+                  </div>
+            </div>
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="#" class="d-inline-block mb-4"><img src="images/jewelry.jpg" alt="Image" class="img-fluid"></a>
+                  <div class="ftco-media-details">
+                    <h3 style="font-size: 30px; font-family:  monospace">JEWELRY</h3>
+                  
+                  </div>
+    
+                </div> 
+              </div>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+              <div class="ftco-media-1">
+                <div class="ftco-media-1-inner">
+                  <a href="#" class="d-inline-block mb-4"><img src="images/shoes.jpg" alt="Image" class="img-fluid"></a>
+                  <div class="ftco-media-details">
+                    <h3 style="font-size: 30px; font-family:  monospace">SHOES</h3>
+                   
+                  </div>
+    
+                </div> 
+              </div>
+            </div>
+
+             <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+            <div class="ftco-media-1">
+              <div class="ftco-media-1-inner">
+                <a href="#" class="d-inline-block mb-4"><img src="images/sweatshirt.jpg" alt="FImageo" class="img-fluid"></a>
+                <div class="ftco-media-details">
+                  <h3 style="font-size: 30px; font-family:  monospace">SWEATSHIRTS</h3>
+                
+                </div>
+  
+              </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+   
+
+    <section class="site-section" id="about-section">
+      <div class="container">
+        
+        <div class="row large-gutters">
+          <div class="col-lg-6 mb-5">
+
+              <div class="owl-carousel slide-one-item with-dots">
+                  <div><img src="images/img_1.jpg" alt="Image" class="img-fluid"></div>
+                </div>
+
+          </div>
+          <div class="col-lg-6 ml-auto">
+            
+            <h2 class="section-title mb-3">My Website</h2>
+                <p class="lead">BLA BLA BLA BLA BLA BLA </p>
+                <p>THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE . THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE
+                THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE THIS PAGE WOULD BE DEDICATED TO TALK ABOUT THE WEBSITE ... </p>
+                <p><a href="#" class="btn btn-primary mr-2 mb-2">Learn More</a></p>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+    
+        <section class="site-section bg-light bg-image" id="contact-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <h2 class="section-title mb-3">Conact Us</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-7 mb-5">
+
+            
+
+            <form role="form" method="post" id="reused_form" class="p-5 bg-white">
+              <h2 class="h4 text-black mb-5">Get In Touch</h2> 
+
+              <div class="row form-group">
+                <div class="col-md-6 mb-3 mb-md-0">
+                  <label class="text-black" for="firstname">First Name</label>
+                  <input type="text" class="form-control" id="firstname" name="firstname" maxlength="50">
+                </div>
+                <div class="col-md-6">
+                  <label class="text-black" for="lastname">Last Name</label>
+                  <input type="text" class="form-control" id="lastname" name="lastname" maxlength="50">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                
+                <div class="col-md-12">
+                  <label class="text-black" for="email">Email</label> 
+                  <input type="email" class="form-control" id="email" name="email" maxlength="50">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                
+                <div class="col-md-12">
+                  <label class="text-black" for="subject">Subject</label> 
+                  <input type="text" name="subject" class="form-control" id="subject" name="subject" maxlength="100">
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="text-black" for="message">Message</label> 
+                  <textarea type="textarea" name="message" id="message" cols="30" rows="7" maxlength="6000" class="form-control" placeholder="Write your notes or questions here..."></textarea>
+                </div>
+              </div>
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" value="Send Message" class="btn btn-primary btn-md text-white">
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+          <div class="col-md-5">
+            
+            <div class="p-4 mb-3 bg-white">
+              <p class="mb-0 font-weight-bold">Address</p>
+              <p class="mb-4">Agadir</p>
+
+              <p class="mb-0 font-weight-bold">Phone</p>
+              <p class="mb-4"><a href="#">+212666666666</a></p>
+
+              <p class="mb-0 font-weight-bold">Email Address</p>
+              <p class="mb-0"><a href="#">myemail@gmail.com</a></p>
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    
+
+
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="row">
+              <div class="col-md-5">
+                <h2 class="footer-heading mb-4">About Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
+              </div>
+              <div class="col-md-3 mx-auto">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-4">
+              <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+            <form action="#" method="post" class="footer-subscribe">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary text-black" type="button" id="button-addon2">Send</button>
+                </div>
+              </div>
+            </form>  
+            </div>
+            
+            <div class="">
+              <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+            </div>
+
+
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p class="copyright">
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+            </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+  </div> 
+
+  <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a> 
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
